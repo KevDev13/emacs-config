8cabbcf728144ef15fbdec91cbe9355063cb2cca
Kevin Garner <kevin@kgar.net>
2020-12-11 22:00:47 -0600

    fixed line numbers

--- init.el
+++ init.el
@@ -1,3 +1,18 @@
+;; no splash screen on startup
+(setq inhibit-splash-screen t)
+
+;; line numbers and such
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode))
+(setq display-fill-column-indicator-column 80)
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;;(require 'fill-column-indicator)
+;;(set-face-attribute 'fill-column-attribute nil :foreground "grey90")
+(setq display-fill-column-indicator t)
+(setq display-fill-column-indicator-character "#")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -11,16 +26,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; no splash screen on startup
-(setq inhibit-splash-screen t)
-
-;; line numbers and such
-(setq line-number-mode t)
-(setq column-number-mode t)
-(setq fill-column 80)
-;;(set-face-attribute 'fill-column-attribute nil :foreground "grey90")
-;;(Require 'fill-column-indicator)
 
 ;;(require 'package)
 ;;(add-to-list 'package-archives
