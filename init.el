4db079fb7055c49ffa3ce8530e98227efb2ec6d6
Kevin Garner <kevin@kgar.net>
2020-12-11 22:08:01 -0600

    added highlights when going over 80 chars in a column

--- init.el
+++ init.el
@@ -4,15 +4,21 @@
 ;; line numbers and such
 (when (version<= "26.0.50" emacs-version)
   (global-display-line-numbers-mode))
-(setq display-fill-column-indicator-column 80)
+;;(setq display-fill-column-indicator-column 80)
 (setq line-number-mode t)
 (setq column-number-mode t)
 
+;; going over 80 columns == not good
+(require 'whitespace)
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
+
 ;;(require 'fill-column-indicator)
 ;;(set-face-attribute 'fill-column-attribute nil :foreground "grey90")
-(setq display-fill-column-indicator t)
-(setq display-fill-column-indicator-character "#")
+;;(setq display-fill-column-indicator t)
+;;(setq display-fill-column-indicator-character "#")
 
+;; Rust stuff
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -36,4 +42,5 @@
 (add-hook 'rust-mode-hook
           (lambda () (setq indent-tabs-mode nil)))
 
+;; custom key bindings
 (global-set-key [f5] 'revert-buffer)
